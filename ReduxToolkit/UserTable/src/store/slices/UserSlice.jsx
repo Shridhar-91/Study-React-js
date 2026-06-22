dd093833aa5d07af7db3676bf35f8817a3bb1760
Shridhar-91 <meshridhar91@gmail.com>
2026-06-23 02:22:59 +0530

    Create a new ReduxToolkit  project

---
+++ ReduxToolkit/UserTable/src/store/slices/UserSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+
+const UserSlice = createSlice({
+    name: "user",
+    initialState: [],
+    reducers: {
+        addUser(state,action){},
+        removeUser(state,action){},
+        deleteUser(state,action){},
+    },
+});
+
+export default UserSlice.reducer; 
