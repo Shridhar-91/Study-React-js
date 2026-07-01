1747b53750de5adcb3deba314e5019fa68137e88
Shridhar-91 <meshridhar91@gmail.com>
2026-07-01 12:56:07 +0530

    update the code regarding AutoComplete Search Bar

--- AutoCompleteSearchBar/src/App.jsx
+++ AutoCompleteSearchBar/src/App.jsx
@@ -5,18 +5,28 @@ function App() {
 
   const [results, setResults] =useState([]);
   const [input, setInput] =useState("");
+  const [cache, setCache] =useState({});
 
   const fetchData = async () => { 
+    if(cache[input]){
+      console.log("Returned cache : ", input);
+      setResults(cache[input]);
+      return;
+    }
+    console.log("API Input : ", input)
     const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
     const json = await data.json();
     setResults(json?.recipes);
+    setCache((prev)=>({...prev, [input]:json?.recipes}))
   };
 
-  useEffect(()=>{
-    fetchData();
-  },[input])
-
-  console.log(results)
+  useEffect(() => {
+    // fetchData();
+    const timer = setTimeout(fetchData, 300);
+    return() => {
+      clearTimeout(timer);
+    };
+  },[input]);
 
   return (
       <div className="app">
@@ -30,9 +40,9 @@ function App() {
             onChange={(e)=> setInput(e.target.value)}
           />
           <div className="results-container">
-            {results.map((r) =>  {
-              <span className="result" key ={r.id}>{r.name}</span>
-            })}
+            {results.map((r) => (
+              <span className="result" key={r.id}>{r.name}</span>
+            ))}
           </div> 
         </div>
       </div>
